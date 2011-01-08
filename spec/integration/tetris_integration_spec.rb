720a1036e928ca993cc1ab74ac9b0236ea87d656
Eric Teubert <ericteubert@googlemail.com>
2011-01-08 19:01:57 +0100

    formulate first basic integration test

--- spec/integration/tetris_integration_spec.rb
+++ spec/integration/tetris_integration_spec.rb
@@ -1,4 +1,14 @@
+require "tetris"
 
 describe "tetris command line game" do
-  it "can be played"
+  
+  it "should be possible to place a tetromino" do
+    game = Tetris::Game.new
+    board = game.board
+    
+    board.current_tetromino.drop
+
+    game.board.should_not be_empty
+  end
+  
 end
