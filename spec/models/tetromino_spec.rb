3bb0b98fea4c4fd404263c182dc64d31fcde77ac
Eric Teubert <ericteubert@googlemail.com>
2011-01-08 17:43:01 +0100

    introduce tetramino types

--- spec/models/tetromino_spec.rb
+++ spec/models/tetromino_spec.rb
@@ -1,6 +1,10 @@
 module Tetris
   class Tetromino
     
+    def type
+      'I'
+    end
+    
     def to_s
       '****'
     end
@@ -12,5 +16,11 @@ describe Tetris::Tetromino do
   it "should be printable" do
     tm = Tetris::Tetromino.new
     tm.to_s.should eql("****")
+  end
+  
+  it "should have a type which is a letter representing the form" do
+      tm = Tetris::Tetromino.new 
+      tm.type.should eql("I")
+      
   end
 end
