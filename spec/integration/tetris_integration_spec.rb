267370989345f99d48134c9ab60834c15c965e42
Eric Teubert <ericteubert@googlemail.com>
2011-01-08 16:43:51 +0100

    skeleton and basic test

---
+++ spec/integration/tetris_integration_spec.rb
@@ -0,0 +1,4 @@
+
+describe "tetris command line game" do
+  it "can be played"
+end
---
+++ spec/models/game_spec.rb
@@ -0,0 +1,13 @@
+module Tetris
+  class Game
+
+  end
+end
+
+describe Tetris::Game do
+  it "should be creatable" do
+    game = Tetris::Game.new
+  end
+  
+  it "should have dimensions"
+end
