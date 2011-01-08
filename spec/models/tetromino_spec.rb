038eeea62bbdc010291a8db7633c769c4ecaad58
Eric Teubert <ericteubert@googlemail.com>
2011-01-08 18:24:42 +0100

    refactor tm tests

--- spec/models/tetromino_spec.rb
+++ spec/models/tetromino_spec.rb
@@ -25,24 +25,25 @@ module Tetris
 end
 
 describe Tetris::Tetromino do
+  
+  before(:each) do
+    @tm = Tetris::Tetromino.new
+  end
+  
   it "should be printable" do
-    tm = Tetris::Tetromino.new
-    tm.to_s.should eql("****")
+    @tm.to_s.should eql("****")
   end
   
   it "should have a type which is a letter representing the form" do
-      tm = Tetris::Tetromino.new 
-      tm.type.should eql("I")
+      @tm.type.should eql("I")
   end
   
   it "should be possible to change the type" do
-      tm = Tetris::Tetromino.new
-      tm.type = 'S'
-      tm.to_s.should eql(" **\n** ")
+      @tm.type = 'S'
+      @tm.to_s.should eql(" **\n** ")
   end
   
   it "should not be possible to set an invalid type" do
-    tm = Tetris::Tetromino.new
-    lambda { tm.type = 'Q' }.should raise_error  
+    lambda { @tm.type = 'Q' }.should raise_error  
   end
 end
