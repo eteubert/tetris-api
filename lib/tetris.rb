76058e2fbdf5071afbc9425c121d97c9ff0f4da8
Eric Teubert <ericteubert@googlemail.com>
2011-01-08 18:56:28 +0100

    refactor: move tm to library

--- lib/tetris.rb
+++ lib/tetris.rb
@@ -31,4 +31,28 @@ module Tetris
     end
     
   end
+  
+  class Tetromino
+    
+    attr_reader :type
+    
+    VALID_TYPES = %w[I J S T Z L O]
+    
+    def initialize
+      @type = 'I'
+    end
+    
+    def type=(letter)
+      raise 'invalid tetromino type' unless VALID_TYPES.include? letter
+      @type = letter
+    end
+    
+    def to_s
+      case @type
+      when 'I' then '****'
+      when 'S' then " **\n** "  
+      end
+    end
+    
+  end
 end
--- spec/models/tetromino_spec.rb
+++ spec/models/tetromino_spec.rb
@@ -1,29 +1,3 @@
-module Tetris
-  class Tetromino
-    
-    attr_reader :type
-    
-    VALID_TYPES = %w[I J S T Z L O]
-    
-    def initialize
-      @type = 'I'
-    end
-    
-    def type=(letter)
-      raise 'invalid tetromino type' unless VALID_TYPES.include? letter
-      @type = letter
-    end
-    
-    def to_s
-      case @type
-      when 'I' then '****'
-      when 'S' then " **\n** "  
-      end
-    end
-    
-  end
-end
-
 describe Tetris::Tetromino do
   
   before(:each) do
