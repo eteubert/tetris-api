0d189e103c273ad8926e502fca92b8bcec1402b3
Eric Teubert <ericteubert@googlemail.com>
2011-01-18 16:01:30 +0100

    slightly change integration test to make it more minimal

--- spec/integration/tetris_integration_spec.rb
+++ spec/integration/tetris_integration_spec.rb
@@ -5,10 +5,10 @@ describe "tetris command line game" do
   it "should be able to determine all possible placements of the current tetromino" do
     @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 3, :height => 4}))
     @board = @game.board
-    @tm = Tetris::Tetromino.new('I')
+    @tm = Tetris::Tetromino.new('O')
     @board.current_tetromino = @tm
     @possibilities = @board.generate_possibilities_for_current_tetromino
-    @possibilities.count.should be 4
+    @possibilities.should have(4).items
   end
   
 end
