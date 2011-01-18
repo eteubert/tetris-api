956267f450a0629de018c634515c65f51adcffa9
Eric Teubert <ericteubert@googlemail.com>
2011-01-18 15:08:50 +0100

    write first real integration test

--- lib/tetris/board.rb
+++ lib/tetris/board.rb
@@ -1,13 +1,19 @@
 module Tetris
   
   class Board
-    attr_reader :dimensions, :lines_cleared, :current_tetromino, :next_tetromino
+    attr_reader :dimensions, :lines_cleared, :next_tetromino
+    attr_accessor :current_tetromino
     
     def initialize(dimensions = nil)
       @dimensions = dimensions || Dimensions.new
       @lines_cleared = 0
       @current_tetromino = Tetromino.generate_random
       @next_tetromino = Tetromino.generate_random
+    end
+    
+    def generate_possibilities_for_current_tetromino
+      # FIXME just some boilerplate to get started
+      [1,2,3,4]
     end
     
   end
--- spec/integration/tetris_integration_spec.rb
+++ spec/integration/tetris_integration_spec.rb
@@ -2,4 +2,13 @@ require "tetris"
 
 describe "tetris command line game" do
   
+  it "should be able to determine all possible placements of the current tetromino" do
+    @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 3, :height => 4}))
+    @board = @game.board
+    @tm = Tetris::Tetromino.new('I')
+    @board.current_tetromino = @tm
+    @possibilities = @board.generate_possibilities_for_current_tetromino
+    @possibilities.count.should be 4
+  end
+  
 end
