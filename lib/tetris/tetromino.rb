f52afd8437c38cc0a3a6ce34b0ce5f0fa380e8d2
Eric Teubert <ericteubert@googlemail.com>
2011-01-18 15:49:55 +0100

    introduce tetromino letter fields

--- lib/tetris/tetromino.rb
+++ lib/tetris/tetromino.rb
@@ -2,24 +2,194 @@ module Tetris
   
   class Tetromino
     
-    attr_reader :type
+    attr_reader :type, :type_letter
     
     VALID_TYPES = %w[I J S T Z L O]
     
+    TYPE_I = [
+      [
+        [0,0,0,0],
+        [1,2,1,1],
+        [0,0,0,0],
+        [0,0,0,0]
+      ],
+      [
+        [0,1,0,0],
+        [0,2,0,0],
+        [0,1,0,0],
+        [0,1,0,0]
+      ]
+    ]
+    
+    TYPE_O = [
+      [
+        [0,0,0,0],
+        [0,2,1,0],
+        [0,1,1,0],
+        [0,0,0,0]
+      ]
+    ]
+    
+    TYPE_L = [
+      [
+        [0,0,0,0],
+        [0,1,0,0],
+        [0,2,0,0],
+        [0,1,1,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,0,1,0],
+        [1,2,1,0],
+        [0,0,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [1,1,0,0],
+        [0,2,0,0],
+        [0,1,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,0,0,0],
+        [1,2,1,0],
+        [1,0,0,0]
+      ]
+    ]
+    
+    TYPE_J = [
+      [
+        [0,0,0,0],
+        [0,1,0,0],
+        [0,2,0,0],
+        [1,1,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,0,0,0],
+        [1,2,1,0],
+        [0,0,1,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,1,1,0],
+        [0,2,0,0],
+        [0,1,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,0,0,0],
+        [1,2,1,0],
+        [1,0,0,0]
+      ]
+    ]
+    
+    TYPE_S = [
+      [
+        [0,0,0,0],
+        [0,1,1,0],
+        [1,2,0,0],
+        [0,0,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [1,0,0,0],
+        [1,2,0,0],
+        [0,1,0,0]
+      ]
+    ]
+    
+    TYPE_Z = [
+      [
+        [0,0,0,0],
+        [1,1,0,0],
+        [0,2,1,0],
+        [0,0,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,1,0,0],
+        [1,2,0,0],
+        [1,0,0,0]
+      ]
+    ]
+    
+    TYPE_T = [
+      [
+        [0,0,0,0],
+        [0,1,0,0],
+        [1,2,1,0],
+        [0,0,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,1,0,0],
+        [1,2,0,0],
+        [0,1,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,0,0,0],
+        [1,2,1,0],
+        [0,1,0,0]
+      ],
+      [
+        [0,0,0,0],
+        [0,1,0,0],
+        [0,2,1,0],
+        [0,1,0,0]
+      ]
+    ]
+    
+    
     def initialize(type = nil)
-      @type = type || 'I'
+      @type_letter = type || 'I'
+      @rotation_index = 0
     end
     
-    def type=(letter)
+    def type_letter=(letter)
       raise 'invalid tetromino type' unless VALID_TYPES.include? letter
-      @type = letter
+      
+      @type = case letter
+        when "I" then TYPE_I
+        when "J" then TYPE_J
+        when "S" then TYPE_S
+        when "T" then TYPE_T
+        when "Z" then TYPE_Z
+        when "L" then TYPE_L
+        when "O" then TYPE_O
+      end
+      
+      @type_letter = letter
     end
     
     def to_s
-      case @type
-      when 'I' then '****'
+      case @type_letter
+      when 'I' then display(TYPE_I[@rotation_index])
       when 'S' then " **\n** "  
       end
+    end
+    
+    def display(tetromino)
+      result = ''
+      
+      tetromino.each do |row|
+        row.each do |block|
+          if block > 0
+            result << '*'
+          else
+            result << ' '
+          end
+        end
+        result << "\n"
+      end
+      
+      result
+    end
+    
+    def rotate
+      @rotation_index = (@rotation_index + 1) % TYPE_I.length
+      self
     end
     
     def self.generate_random
--- spec/models/tetromino_spec.rb
+++ spec/models/tetromino_spec.rb
@@ -7,23 +7,33 @@ describe Tetris::Tetromino do
   end
   
   it "should be printable" do
-    @tm.to_s.should eql("****")
+    @tm.to_s.should eql("    \n****\n    \n    \n")
   end
   
   it "should have a type which is a letter representing the form" do
-      @tm.type.should eql("I")
+      @tm.type_letter.should eql("I")
   end
   
   it "should be possible to change the type" do
-      @tm.type = 'S'
+      @tm.type_letter = 'S'
       @tm.to_s.should eql(" **\n** ")
   end
   
   it "should not be possible to set an invalid type" do
-    lambda { @tm.type = 'Q' }.should raise_error  
+    lambda { @tm.type_letter = 'Q' }.should raise_error  
   end
   
   it "should be possible to get a random tetromino" do
     Tetris::Tetromino.generate_random.should be_kind_of Tetris::Tetromino
+  end
+  
+  it "should be possible to rotate tetrominos" do
+    @tm = Tetris::Tetromino.new('I')
+    @tm.rotate.to_s.should eql(" *  \n *  \n *  \n *  \n")
+  end
+  
+  it "should be possible to roate more often than there are states" do
+    @tm = Tetris::Tetromino.new('I')
+    @tm.rotate.rotate.to_s.should eql(@tm.to_s)
   end
 end
