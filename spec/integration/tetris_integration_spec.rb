25a324ecef70947a1c7862ab443da99e5f97dab6
Eric Teubert <ericteubert@googlemail.com>
2011-01-18 14:50:06 +0100

    it is not necessary to drop tetrominos

--- spec/integration/tetris_integration_spec.rb
+++ spec/integration/tetris_integration_spec.rb
@@ -2,13 +2,4 @@ require "tetris"
 
 describe "tetris command line game" do
   
-  it "should be possible to place a tetromino" do
-    game = Tetris::Game.new
-    board = game.board
-    
-    board.current_tetromino.drop
-
-    game.board.should_not be_empty
-  end
-  
 end
