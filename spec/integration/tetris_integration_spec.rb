745af7dccaff409461999720346d4f2a901fdb34
Eric Teubert <ericteubert@googlemail.com>
2011-01-24 15:01:04 +0100

    small refactoring

--- spec/integration/tetris_integration_spec.rb
+++ spec/integration/tetris_integration_spec.rb
@@ -2,40 +2,46 @@ require "tetris"
 
 describe "tetris command line game" do
   
-  it "should be able to determine all possible placements of the current tetromino" do
-    @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 3, :height => 4}))
-    @board = @game.board
-    @tm = Tetris::Tetromino.new('O')
-    @board.current_tetromino = @tm
-    @possibilities = @board.generate_possibilities_for_current_tetromino
-    @possibilities.should have(2).items
+  describe "first tetromino placement" do
+    it "should be able to determine all possible placements of the current tetromino" do
+      @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 3, :height => 4}))
+      @board = @game.board
+      @tm = Tetris::Tetromino.new('O')
+      @board.current_tetromino = @tm
+      @possibilities = @board.generate_possibilities_for_current_tetromino
+      @possibilities.should have(2).items
+    end
+
+    it "should be possible to place a sqare tetromino on a field of height 2" do
+      @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 4, :height => 2}))
+      @board = @game.board
+      @tm = Tetris::Tetromino.new('O')
+      @board.current_tetromino = @tm
+      @possibilities = @board.generate_possibilities_for_current_tetromino
+      @possibilities.should have(3).items
+    end
+
+    it "should rotate tetrominos when placing them" do
+      @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 4, :height => 4}))
+      @board = @game.board
+      @tm = Tetris::Tetromino.new('I')
+      @board.current_tetromino = @tm
+      @possibilities = @board.generate_possibilities_for_current_tetromino_including_variants
+      @possibilities.should have(5).items    
+    end
+
+    it "place a T tetromino", :current => true do
+      @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 4, :height => 4}))
+      @board = @game.board
+      @tm = Tetris::Tetromino.new('T')
+      @board.current_tetromino = @tm
+      @possibilities = @board.generate_possibilities_for_current_tetromino_including_variants
+      @possibilities.should have(10).items
+    end
   end
   
-  it "should be possible to place a sqare tetromino on a field of height 2" do
-    @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 4, :height => 2}))
-    @board = @game.board
-    @tm = Tetris::Tetromino.new('O')
-    @board.current_tetromino = @tm
-    @possibilities = @board.generate_possibilities_for_current_tetromino
-    @possibilities.should have(3).items
-  end
-  
-  it "should rotate tetrominos when placing them" do
-    @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 4, :height => 4}))
-    @board = @game.board
-    @tm = Tetris::Tetromino.new('I')
-    @board.current_tetromino = @tm
-    @possibilities = @board.generate_possibilities_for_current_tetromino_including_variants
-    @possibilities.should have(5).items    
-  end
-  
-  it "place a T tetromino", :current => true do
-    @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 4, :height => 4}))
-    @board = @game.board
-    @tm = Tetris::Tetromino.new('T')
-    @board.current_tetromino = @tm
-    @possibilities = @board.generate_possibilities_for_current_tetromino_including_variants
-    @possibilities.should have(10).items
+  describe "tetromino on non-empty board" do
+    
   end
   
 end
