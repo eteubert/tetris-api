c1b755791927a97693d81f01bb84e2deb0d846c1
Eric Teubert <ericteubert@googlemail.com>
2011-01-24 12:31:21 +0100

    basic tetromino placement

--- spec/integration/tetris_integration_spec.rb
+++ spec/integration/tetris_integration_spec.rb
@@ -11,4 +11,13 @@ describe "tetris command line game" do
     @possibilities.should have(2).items
   end
   
+  it "should be possible to place a sqare tetromino on a field of height 2" do
+    @game = Tetris::Game.new(Tetris::Dimensions.new({:width => 4, :height => 2}))
+    @board = @game.board
+    @tm = Tetris::Tetromino.new('O')
+    @board.current_tetromino = @tm
+    @possibilities = @board.generate_possibilities_for_current_tetromino
+    @possibilities.should have(3).items
+  end
+  
 end
