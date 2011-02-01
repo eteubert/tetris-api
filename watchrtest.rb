ab5f837d9a4b149cacbd4e92aa48381efcb6e272
Eric Teubert <ericteubert@googlemail.com>
2011-02-01 16:52:28 +0100

    add watchr gem for continuous testing

---
+++ watchrtest.rb
@@ -0,0 +1,22 @@
+# run specs when they change
+watch('spec/.*/.*\.rb') do |match|
+  exec = "rspec -c #{match[0]}"
+  puts exec
+  system(exec)
+end
+
+# run corresponding specs for tetris libs
+watch('lib/tetris/(.*)\.rb') do |match| 
+  exec = "rspec -c spec/models/#{match[1]}_spec.rb"
+  puts exec
+  system(exec)
+end
+
+# Ctrl-\
+Signal.trap('QUIT') do
+  puts " --- Running all tests ---\n\n"
+  system("rspec -c spec")
+end
+
+# Ctrl-C
+Signal.trap('INT') { abort("\n") }
