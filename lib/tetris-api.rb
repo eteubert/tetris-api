f84a475b5d003e572d4807c11d308e1a1d7d5fe8
Eric Teubert <ericteubert@googlemail.com>
2011-02-08 13:20:00 +0100

    oups, name fuckup. quick workaround for now

--- lib/tetris-api.rb
+++ lib/tetris-api.rb
@@ -3,3 +3,11 @@ module Tetris
     # Your code goes here...
   end
 end
+
+require 'active_support/core_ext'
+
+require 'tetris/board'
+require 'tetris/board_rating'
+require 'tetris/dimensions'
+require 'tetris/game'
+require 'tetris/tetromino'
